b2e5113e8f9843429b5eecdb52005f472b97d489
Tabinka <tabicode@protonmail.com>
2022-01-15 12:08:23 +0100

    adding stripe and env

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreateProductForm, RegisterForm, LoginForm
-
 import stripe
+from decouple import config
 
 # This is a sample test API key. Sign in to see examples pre-filled with your key.
 
-stripe.api_key = 'sk_test_RXHltS2OKzISvxWQ7NmRN57i001oa6x7o4'
-
+stripe.api_key = config('stripe_api')
+CART = []
 YOUR_DOMAIN = 'http://127.0.0.1:5000'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "dadadadnad22393DNdnsandas"
+app.config['SECRET_KEY'] = config('secret_key')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -45,6 +45,8 @@ class User(UserMixin, db.Model, Base):
 class Product(db.Model, Base):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
+    stripe_id = db.Column(db.String(250), unique=True, nullable=False)
+    stripe_price_id = db.Column(db.String(250), unique=True, nullable=False)
     name = db.Column(db.String(250), unique=True, nullable=False)
     alias = db.Column(db.String(250), nullable=False)
     price = db.Column(db.Integer, nullable=False)
@@ -76,6 +78,27 @@ def inject_category():
         name.append(cat.name)
         alias.append(cat.alias)
     return dict({"name": name, "alias": alias})
+
+
+@app.context_processor
+def minicart():
+    cart = CART
+    name = []
+    alias = []
+    price = []
+    qty = []
+    total = 0
+    image = []
+    if len(cart) > 0:
+        for cart_item in cart:
+            prod = Product.query.filter_by(id=cart_item["prod_id"]).first()
+            image.append(prod.image)
+            name.append(cart_item["name"])
+            alias.append(cart_item["alias"])
+            price.append(cart_item["price"])
+            qty.append(cart_item["qty"])
+            total += (cart_item["price"] * cart_item["qty"])
+    return dict({"cart_name": name, "cart_alias": alias, "cart_price": price, "cart_qty": qty, "items": len(cart), "cart_img": image, "total": total})
 
 
 @app.context_processor
@@ -179,9 +202,15 @@ def admin_area():
 def add_new_product():
     form = CreateProductForm()
     if form.validate_on_submit():
+        stripe_product = stripe.Product.create(name=form.name.data)
+        stripe_product_price = stripe.Price.create(unit_amount=int(form.price.data), currency="czk",
+                                                   product=stripe_product["id"])
+        alias = form.name.data.replace(" ", "-").lower()
         new_product = Product(
             name=form.name.data,
-            alias=form.name.data,
+            stripe_id=stripe_product["id"],
+            stripe_price_id=stripe_product_price["id"],
+            alias=alias,
             price=form.price.data,
             description=form.description.data,
             stock=form.stock.data,
@@ -206,8 +235,20 @@ def edit_product(product_id):
         description=product.description
     )
     if edit_form.validate_on_submit():
+        if product.price != edit_form.price.data:
+            stripe.Price.modify(
+                product.stripe_price_id,
+                active=False,
+            )
+            new_product_price = stripe.Price.create(
+                unit_amount=int(edit_form.price.data),
+                currency="czk",
+                product="prod_KnMfuKPBAoT8GG",
+            )
+            product.stripe_price_id = new_product_price["id"]
+        alias = edit_form.name.data.replace(" ", "-").lower()
         product.name = edit_form.name.data
-        product.alias = edit_form.name.data
+        product.alias = alias
         product.image = edit_form.image.data
         product.price = edit_form.price.data
         product.stock = edit_form.stock.data
@@ -227,39 +268,100 @@ def delete_product(product_id):
 
 
 # Stripe Checkout
+# TODO: Style cancel.html and success.html
 
-@app.route('/create-checkout-session', methods=['POST'])
+@app.route('/add-to-cart', methods=["POST", "GET"])
+def add_to_cart():
+    global CART
+    qty = int(request.form.get("qty"))
+    price = int(request.form.get("price"))
+    prod_alias = request.form.get("alias")
+    prod_name = request.form.get("name")
+    id = request.form.get("prod_id")
+    if len(CART):
+        for item in CART:
+            if item["prod_id"] == id:
+                item["qty"] += qty
+            else:
+                CART.append({"alias": prod_alias, "name": prod_name, "price": price, "qty": qty, "prod_id": id})
+    else:
+        CART.append({"alias": prod_alias, "name": prod_name, "price": price, "qty": qty, "prod_id": id})
+    print(CART)
+    return redirect(url_for("show_product", alias=prod_alias))
+
+
+@app.route('/cancel')
+def cancel():
+    return render_template("cancel.html")
+
+
+@app.route('/success')
+def success():
+    CART .clear()
+    return render_template("success.html")
+
+
+@app.route('/create-checkout-session', methods=['POST', "GET"])
 def create_checkout_session():
+    print(CART)
     try:
-
+        items = []
+        for cart_item in CART:
+            items.append({'price_data': {'currency': 'czk', 'product_data': {'name': cart_item["name"]}, 'unit_amount': cart_item["price"]},
+                'quantity': cart_item["qty"]})
         checkout_session = stripe.checkout.Session.create(
-
-            line_items=[
-
+            payment_method_types=['card'],
+            shipping_address_collection={
+                'allowed_countries': ['CZ', 'SK', 'DE'],
+            },
+            shipping_options=[
                 {
-
-                    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-
-                    'price': '{{PRICE_ID}}',
-
-                    'quantity': 1,
-
+                    'shipping_rate_data': {
+                        'type': 'fixed_amount',
+                        'fixed_amount': {
+                            'amount': 0,
+                            'currency': 'czk',
+                        },
+                        'display_name': 'Free shipping',
+                        # Delivers between 5-7 business days
+                        'delivery_estimate': {
+                            'minimum': {
+                                'unit': 'business_day',
+                                'value': 5,
+                            },
+                            'maximum': {
+                                'unit': 'business_day',
+                                'value': 7,
+                            },
+                        }
+                    }
                 },
-
+                {
+                    'shipping_rate_data': {
+                        'type': 'fixed_amount',
+                        'fixed_amount': {
+                            'amount': 1500,
+                            'currency': 'czk',
+                        },
+                        'display_name': 'Next day air',
+                        # Delivers in exactly 1 business day
+                        'delivery_estimate': {
+                            'minimum': {
+                                'unit': 'business_day',
+                                'value': 1,
+                            },
+                            'maximum': {
+                                'unit': 'business_day',
+                                'value': 1,
+                            },
+                        }
+                    }
+                },
             ],
-
-            payment_method_types=[
-
-                'card',
-
-            ],
-
+            line_items=items,
             mode='payment',
-
-            success_url=YOUR_DOMAIN + '/success.html',
-
-            cancel_url=YOUR_DOMAIN + '/cancel.html',
-
+            success_url=YOUR_DOMAIN + '/success',
+            cancel_url=YOUR_DOMAIN + '/cancel',
         )
 
     except Exception as e:
